354f59f453912f9bb790d95e68062525ce19ef40
NekoNeko6996 <lqm231231@gmail.com>
2025-12-13 09:11:01 +0700

    nutritionist dashboard

--- src/main/java/com/group02/zaderfood/controller/AdminController.java
+++ src/main/java/com/group02/zaderfood/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.group02.zaderfood.controller;
 
 import com.group02.zaderfood.dto.AdminDashboardDTO;
+import com.group02.zaderfood.dto.NutritionistDashboardDTO;
 import com.group02.zaderfood.service.AdminService;
 import com.group02.zaderfood.service.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +20,20 @@ public class AdminController {
 
     @GetMapping("/dashboard")
     public String dashboard(Model model, @AuthenticationPrincipal CustomUserDetails currentUser) {
-        // Lấy dữ liệu thống kê
+        if (currentUser.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_NUTRITIONIST") || a.getAuthority().equals("NUTRITIONIST"))) {
+
+            // Gọi logic tính toán RIÊNG
+            NutritionistDashboardDTO nutriStats = adminService.getNutritionistStats();
+            model.addAttribute("stats", nutriStats);
+
+            // Trả về view RIÊNG
+            return "admin/nutritionist_dashboard";
+        }
+
+        // Logic cho Admin hệ thống (Giữ nguyên cái cũ)
         AdminDashboardDTO stats = adminService.getDashboardStats();
-        
-        // Đẩy sang View
         model.addAttribute("stats", stats);
-        
-        // Trả về file admin_dashboard.html (đặt trong folder templates/admin/)
         return "admin/admin_dashboard";
     }
 }
